aa1c41bd9069f58f018f14cb05e578d50ebee3d6
ali2130176 <ali2130176@gmail.com>
2026-05-22 00:21:07 +1200

    updated 1
    iam tryng to add my code idea to the project

--- game.cpp
+++ game.cpp
@@ -2,6 +2,11 @@
 #include <iostream>
 
 // Initialise (called once at start)
+// create a circle for the bird
+// create 2 pillars with a space between them make it so they have random heights
+// create 2 more pillars on top of the first pillars use random numbers to start from height on top of the first two pillars make this height between top and buttom pillar random
+// make the pillars move towards left 
+// allow the ball to move uo when press sapce button 
 void init() {
 
 }
