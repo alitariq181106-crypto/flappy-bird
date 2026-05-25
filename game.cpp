2a3b7265e7144340cd0d737b56199b7c3bd49c61
ali2130176 <ali2130176@gmail.com>
2026-05-26 00:53:11 +1200

    updated random num
    updated random num

--- game.cpp
+++ game.cpp
@@ -34,7 +34,10 @@ float jumpForce;
 float pipeSpeed;
 int score;
 // provides a random number between the two numbers provided
-int random(int num1, int num2){}
+int random(int num1, int num2){
+    int num = (rand()%num2)+num1;
+    return num;
+}
 void init() {
     pipes.clear();
 
@@ -48,11 +51,11 @@ void init() {
     pipeSpeed = 200.0f;
     score = 0;
 
-    pipe1.pos = Vec2 (WINDOW_WIDTH/3,0);
+    pipe1.pos = Vec2 (WINDOW_WIDTH/1.3,0);
     pipe1.vel = Vec2(0,0);
-    pipe1_top.pos = Vec2(WINDOW_WIDTH/3,WINDOW_HEIGHT);
-    pipe2.pos = Vec2 (WINDOW_WIDTH*2/5,0);
-    pipe2_top.pos = Vec2(WINDOW_WIDTH*2/5,WINDOW_WIDTH);
+    pipe1_top.pos = Vec2(WINDOW_WIDTH/1.3,WINDOW_HEIGHT);
+    pipe2.pos = Vec2 (WINDOW_WIDTH/0.35,0);
+    pipe2_top.pos = Vec2(WINDOW_WIDTH/0.35,WINDOW_HEIGHT);
     pipe1.size= Vec2 (50,100);
     pipe2.size= Vec2 (50,500);
     pipe2_top.size= Vec2 (50,800);
@@ -67,7 +70,7 @@ void update(float dt) {
     //if (isKeyPressed(KEY_SPACE)) {
         //bird.vel.y = jumpForce;
     //}
-    Vec2 forward = Vec2(0,-1);
+    Vec2 forward = Vec2(-1,0);
     pipe1.vel = Vec2(forward*pipeSpeed);
     pipe1_top.pos += pipe1.vel *dt;
     pipe2.pos +=pipe1.vel *dt;
@@ -76,13 +79,17 @@ void update(float dt) {
 
     //bird.vel.y = bird.vel.y + gravity * dt;
     //bird.pos.y = bird.pos.y + bird.vel.y * dt;
-    if ((pipe1.pos.x ==0)&&(pipe1_top.pos.x ==0)){
+    if ((pipe1.pos.x <=0)&&(pipe1_top.pos.x <=0)){
         pipe1.size = Vec2(50,random(0,WINDOW_HEIGHT));
         pipe1_top.size = Vec2(50,random(pipe1.size.y+50,WINDOW_HEIGHT));// this fifty can alo be a random number that will be the distance between two pillars
+        pipe1_top.pos = Vec2(WINDOW_WIDTH,WINDOW_HEIGHT);
+        pipe1.pos = Vec2 (WINDOW_WIDTH,0);
     }
-    if ((pipe2.pos.x ==0)&&(pipe2_top.pos.x ==0)){
+    if ((pipe2.pos.x <=0)&&(pipe2_top.pos.x <=0)){
         pipe2.size = Vec2(50,random(0,WINDOW_HEIGHT));
         pipe2_top.size = Vec2(50,random(pipe2.size.y+50,WINDOW_HEIGHT));// this fifty can alo be a random number that will be the distance between two pillars
+        pipe1_top.pos = Vec2(WINDOW_WIDTH,WINDOW_HEIGHT);
+        pipe2.pos = Vec2 (WINDOW_WIDTH,0);
     }
 }
 
