35beeb91e4be2d0f26c637dfcff683ffdb35e1ef
ali2130176 <ali2130176@gmail.com>
2026-05-25 17:59:52 +1200

    UPDATED
    you made a few mistakes the file is missing something i cannot run the game

--- game.cpp
+++ game.cpp
@@ -12,7 +12,7 @@ using namespace std;
 // allow the ball to move uo when press sapce button
 
 struct Bird {
-    Vec2 pos;
+    Vec2 pos,size;
     Vec2 vel;
     float radius;
     Texture texture;
@@ -36,9 +36,10 @@ int score;
 void init() {
     pipes.clear();
 
-    bird.pos = {100, WINDOW_HEIGHT/2};
-    bird.vel = {0, 0};
+    bird.pos = Vec2(100, WINDOW_HEIGHT/2);
+    bird.vel = Vec2(0, 0);
     bird.radius = 15;
+    bird.size = Vec2(10,10);
 
     gravity = 400.0f;
     jumpForce = -250.0f;
@@ -50,12 +51,12 @@ void init() {
 
 // Update Game
 void update(float dt) {
-    if (isKeyPressed(KEY_SPACE)) {
+    //if (isKeyPressed(KEY_SPACE)) {
         bird.vel.y = jumpForce;
-    }
+    //}
 
-    bird.vel.y = bird.vel.y + gravity * dt;
-    bird.pos.y = bird.pos.y + bird.vel.y * dt;
+    //bird.vel.y = bird.vel.y + gravity * dt;
+    //bird.pos.y = bird.pos.y + bird.vel.y * dt;
 
 }
 
@@ -64,7 +65,8 @@ void render(float lag) {
     // Clear Screen
     clear(0, 0, 0);
 
-    fillCircle(bird.pos.x, bird.pos.y, bird.radius);
+    drawCircle(bird.pos,bird.radius,Color::red);
+
 }
 
 // Close the Game
